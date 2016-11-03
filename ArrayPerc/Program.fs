fe82c9d0ef216728cbce2e1764bf0aa4206ec380
Igor Muravev <to.imuravev@gmail.com>
2016-11-03 17:01:11 +0500

    add base func

--- ArrayPerc/Program.fs
+++ ArrayPerc/Program.fs
@@ -1,9 +1,10 @@
 ﻿open System
 
-let N = 10;
-let minValue = -100.0;
-let maxValue = 100.0;
-let rnd = new Random();
+let N = 10
+let t = 100.0
+let minValue = -100.0
+let maxValue = 100.0
+let rnd = new Random()
 
 //#region teachPairInit
 let listInit =
@@ -15,16 +16,17 @@ let listInit =
 let maxL(L) =
     List.max(L)
 
-let rec teachPairs(count) =
+let rec genTeachPairs(count) =
     let bufList = listInit
     let max = maxL(bufList)
     match count with
         | _ when count > 1 ->
-            [(bufList, max)] @ teachPairs(count - 1)
+            [(bufList, max)] @ genTeachPairs(count - 1)
         | _ ->
             [(bufList, max)]  
 //#endregion
 
+//region netwrork
 let initWPerc = 
     [
         for i in 1..N do
@@ -37,8 +39,26 @@ let getNetwork =
             yield initWPerc
     ]
 
+let activateFunc(x:float) =
+    1.0 / (1.0 + exp(-t * x))
+
+let percResult(perc, input) =
+    List.zip perc input |> List.fold (fun acc (fst, snd) -> acc + fst * snd) 0.0
+
+let rec netResult(nw , input) =
+    match nw with
+        | [] -> []
+        | head :: tail ->
+            let res = percResult(head, input) |> activateFunc
+            res :: netResult(tail, input)
+          
+
+//end region
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" getNetwork
+    let network = getNetwork
+    let teachPairs =genTeachPairs(100)
+
+
     0 // return an integer exit code
